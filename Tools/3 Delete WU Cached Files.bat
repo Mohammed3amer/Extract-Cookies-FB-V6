f3b6713280190f4b6f3e1881c6eeb6ac8fd25277
Mohammed3amer <128042204+Mohammed3amer@users.noreply.github.com>
2024-11-24 01:26:51 +0200

    Add files via upload

---
+++ Tools/3 Delete WU Cached Files.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Batch File By Adamx (YouTube.Com/AdamxYT)
+@echo
+net stop wuauserv
+@echo
+net stop UsoSvc
+@echo
+net stop bits
+@echo
+net stop dosvc
+@echo
+echo Deleting Windows Update Files
+rd /s /q C:\Windows\SoftwareDistribution
+md C:\Windows\SoftwareDistribution
+exit
